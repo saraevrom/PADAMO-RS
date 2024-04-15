821763e6e611d2ae13f21f9c374d5a363b2720c8
Roman Saraev <saraevrom@gmail.com>
2024-04-15 17:03:12 +0300

    Added function for creation of function box

--- padamo-api/src/function_operator.rs
+++ padamo-api/src/function_operator.rs
@@ -1,4 +1,5 @@
 use abi_stable::{sabi_trait, std_types::RBox};
+use abi_stable::sabi_trait::prelude::TD_Opaque;
 
 
 #[sabi_trait]
@@ -10,3 +11,6 @@ pub trait DoubleFunctionOperator: Clone+Debug+Sync+Send{
 pub type DoubleFunctionOperatorBox = DoubleFunctionOperator_TO<'static,RBox<()>>;
 
 
+pub fn make_function_box<T,U:DoubleFunctionOperator+'static>(data:U)->DoubleFunctionOperatorBox{
+    DoubleFunctionOperatorBox::from_value(data, TD_Opaque)
+}
