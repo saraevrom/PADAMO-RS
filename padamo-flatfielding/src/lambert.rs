324c0b3993d05c036e0b53bf784afd1abb5575d7
Roman Saraev <saraevrom@gmail.com>
2024-05-05 23:12:22 +0300

    Switched lambertw to newtonian method

--- padamo-flatfielding/src/lambert.rs
+++ padamo-flatfielding/src/lambert.rs
@@ -1,9 +1,12 @@
 use std::f64::consts::E;
 
-pub fn lambertw0(x:f64)->f64{
+fn lambertw0_binary(x:f64, precision:f64)->f64{
+    if x < -1.0/E{
+        return -1.0;
+    }
     let mut left = -1.0;
     let mut right = if x<=E {E} else {x};
-    while right-left>1e-12{
+    while right-left>precision{
         let mid = (left+right)/2.0;
         if mid*mid.exp()>x{
             right = mid;
@@ -13,4 +16,35 @@ pub fn lambertw0(x:f64)->f64{
         }
     }
     right
+}
+
+
+fn lambertw0_newtonian(y:f64,precision:f64)->f64{
+    if y <= -1.0/E{
+        return -1.0;
+    }
+
+    //f(x)=x exp(x) (x<1.0)
+    //f'(x)=(1 + x)*exp(x) (x<1.0)
+
+    let mut prev_x = y+precision*10.0;
+    let mut x = y;
+    while (x-prev_x).abs()>precision{
+        let grad = (1.0 + x)*x.exp();
+        if grad==0.0{
+            panic!("LambertW0: How did we get here?");
+        }
+        let y0 = x*x.exp();
+        let new_x = x+(y-y0)/grad;
+        prev_x = x;
+        x = new_x;
+    }
+    x
+
+}
+
+
+pub fn lambertw0(x:f64)->f64{
+    //lambertw0_binary(x, 1e-12)
+    lambertw0_newtonian(x, 1e-12)
 }
