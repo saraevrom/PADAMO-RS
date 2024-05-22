37ada84a1bcc30b517b62027f1e3ec38caec17e0
Roman Saraev <saraevrom@gmail.com>
2024-05-22 15:39:08 +0300

    modified windows build script for ANN

--- builder-release.bat
+++ builder-release.bat
@@ -3,7 +3,16 @@
 cargo build --release --workspace
 
 cd target/release
+
+if not exist "padamo-neuraltrigger/" mkdir "padamo-neuraltrigger/"
+del padamo-neuraltrigger/*
+move padamoneuraltrigger.dll padamo-neuraltrigger/
+copy -fv ../../padamo-neuraltrigger/*.onnx padamo-neuraltrigger/
+move onnx*.dll padamo-neuraltrigger/
+
 if not exist "plugins/" mkdir "plugins/"
 del padamo_api_macros_internal.dll
 del padamo_iced_forms.dll
 move *.dll plugins
+
+move  padamo-neuraltrigger/ plugins/padamo-neuraltrigger
