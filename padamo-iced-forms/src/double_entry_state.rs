eac7a1fae9acf5b9a21b2e9881da6ed6b82c05f5
Roman Saraev <saraevrom@gmail.com>
2024-10-25 18:20:37 +0300

    Made entry text color also change if data invalid in double_entry_state

--- padamo-iced-forms/src/double_entry_state.rs
+++ padamo-iced-forms/src/double_entry_state.rs
@@ -9,6 +9,16 @@ pub fn errored_text(disp:&str,is_ok:bool)->iced::widget::Text<'_,iced::theme::Th
     txt
 }
 
+pub fn errored_entry<'a,M:Clone,F:Fn(String)->M+'a>(placeholder:&'a str,value:&'a str,is_ok:bool, msg:F)->iced::widget::TextInput<'a,M>{
+    use iced::widget::text_input;
+    let mut entry = text_input(placeholder, value).on_input(msg);
+    if !is_ok{
+        entry = entry.style(|t,s| text_input::Style{
+            value: iced::Color { r: 1.0, g: 0.0, b: 0.0, a: 1.0},..text_input::default(t,s)});
+    }
+    entry
+}
+
 #[derive(Clone,Debug)]
 pub struct EntryState<T:ToString+FromStr>{
     pub parsed_value:T,
@@ -38,15 +48,16 @@ impl<T:ToString+FromStr> EntryState<T>{
         self.is_valid = true;
     }
 
-    pub fn view<'a,Message:Clone+'a, F:Fn(String)->Message+'a> (&'a self,placehoder:&str, msg:F)->iced::widget::TextInput<'a,Message>{
-        iced::widget::text_input(placehoder,&self.string_value).on_input(msg)
+    pub fn view<'a,Message:Clone+'a, F:Fn(String)->Message+'a> (&'a self,placehoder:&'a str, msg:F)->iced::widget::TextInput<'a,Message>{
+        errored_entry(placehoder, &self.string_value, self.is_valid, msg)
+        //iced::widget::text_input(placehoder,&self.string_value).on_input(msg)
     }
 
     pub fn view_label<'a>(&'a self, label:&'a str)->iced::widget::Text<'_,iced::theme::Theme>{
         errored_text(label,self.is_valid)
     }
 
-    pub fn view_row<'a,Message:Clone+ 'a, F:Fn(String)->Message+'a>(&'a self,label:&'a str, placehoder:&str, msg:F)->iced::widget::Row<'a,Message>{
+    pub fn view_row<'a,Message:Clone+ 'a, F:Fn(String)->Message+'a>(&'a self,label:&'a str, placehoder:&'a str, msg:F)->iced::widget::Row<'a,Message>{
         row![
             self.view_label(label),
             self.view(placehoder, msg)
