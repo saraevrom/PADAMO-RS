7a8b47b474053e952ec47300bfe1776be58b6705
Roman Saraev <saraevrom@gmail.com>
2024-11-08 20:05:06 +0300

    Moved time search functionality to api

--- padamo-api/src/lazy_array_operations/mod.rs
+++ padamo-api/src/lazy_array_operations/mod.rs
@@ -89,3 +89,52 @@ where
         LazyArrayOperationBox::from_value(cached, TD_Opaque)
     }
 }
+
+impl LazyTimeSignal{
+    pub fn find_unixtime(&self,unixtime:f64)->usize{
+        let op = self;
+        //let unixtime:f64 = (dt.naive_utc().timestamp_millis() as f64)
+        let mut start:usize = 0;
+        let op_length = op.length();
+        let mut end:usize = op_length;
+        let mut middle:usize = (start+end)/2;
+        if unixtime>op.request_range(end-1,end)[0]{
+            return end-1;
+        }
+        if unixtime<op.request_range(0,1)[0]{
+            return 0;
+        }
+        while start != middle{
+            let item = op.request_range(middle,middle+1)[0];
+            if item<=unixtime{
+                start = middle;
+            }
+            if item>=unixtime{
+                end = middle;
+            }
+            middle = (start+end)/2;
+        }
+        //println!("Datetime search result. req: {}, actual: {}",unixtime, op.request_item(middle));
+        let mut res = middle;
+        if middle>0{
+            let twoval = op.request_range(middle-1,middle+1);
+            if (twoval[0]-unixtime).abs()<(twoval[1]-unixtime).abs(){
+                res = middle-1;
+            }
+        }
+        if middle<op_length-1{
+            let twoval = op.request_range(middle,middle+2);
+            if (twoval[0]-unixtime).abs()>(twoval[1]-unixtime).abs(){
+                res = middle+1;
+            }
+        }
+        res
+    }
+
+    #[cfg(feature = "chrono")]
+    pub fn find_time(&self,dt:chrono::DateTime<chrono::Utc>)->usize{
+        let unixtime:f64 = (dt.naive_utc().and_utc().timestamp_micros() as f64)*1e-6;
+        self.find_unixtime(unixtime)
+    }
+
+}
--- src/time_search.rs
+++ src/time_search.rs
@@ -4,30 +4,46 @@ use chrono::{DateTime, Utc};
 
 pub fn find_unixtime(op:&LazyTimeSignal,unixtime:f64)->usize{
     //let unixtime:f64 = (dt.naive_utc().timestamp_millis() as f64)
-    let mut start:usize = 0;
-    let mut end:usize = op.length();
-    let mut middle:usize = (start+end)/2;
-    if unixtime>op.request_range(end-1,end)[0]{
-        return end-1;
-    }
-    if unixtime<op.request_range(0,1)[0]{
-        return 0;
-    }
-    while start != middle{
-        let item = op.request_range(middle,middle+1)[0];
-        if item<=unixtime{
-            start = middle;
-        }
-        if item>=unixtime{
-            end = middle;
-        }
-        middle = (start+end)/2;
-    }
-    //println!("Datetime search result. req: {}, actual: {}",unixtime, op.request_item(middle));
-    middle
+    // let mut start:usize = 0;
+    // let op_length = op.length();
+    // let mut end:usize = op_length;
+    // let mut middle:usize = (start+end)/2;
+    // if unixtime>op.request_range(end-1,end)[0]{
+    //     return end-1;
+    // }
+    // if unixtime<op.request_range(0,1)[0]{
+    //     return 0;
+    // }
+    // while start != middle{
+    //     let item = op.request_range(middle,middle+1)[0];
+    //     if item<=unixtime{
+    //         start = middle;
+    //     }
+    //     if item>=unixtime{
+    //         end = middle;
+    //     }
+    //     middle = (start+end)/2;
+    // }
+    // //println!("Datetime search result. req: {}, actual: {}",unixtime, op.request_item(middle));
+    // let mut res = middle;
+    // if middle>0{
+    //     let twoval = op.request_range(middle-1,middle+1);
+    //     if (twoval[0]-unixtime).abs()<(twoval[1]-unixtime).abs(){
+    //         res = middle-1;
+    //     }
+    // }
+    // if middle<op_length-1{
+    //     let twoval = op.request_range(middle,middle+2);
+    //     if (twoval[0]-unixtime).abs()>(twoval[1]-unixtime).abs(){
+    //         res = middle+1;
+    //     }
+    // }
+    // res
+    op.find_unixtime(unixtime)
 }
 
 pub fn find_time(op:&LazyTimeSignal,dt:DateTime<Utc>)->usize{
-    let unixtime:f64 = (dt.naive_utc().and_utc().timestamp_micros() as f64)*1e-6;
-    find_unixtime(op, unixtime)
+    // let unixtime:f64 = (dt.naive_utc().and_utc().timestamp_micros() as f64)*1e-6;
+    // find_unixtime(op, unixtime)
+    op.find_time(dt)
 }
