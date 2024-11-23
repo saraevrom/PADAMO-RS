c74b1e9478dda69daa3233486acfe9f483063d08
Roman Saraev <saraevrom@gmail.com>
2024-11-23 18:33:27 +0300

    Made resolution reduction node use less memory

--- padamo-signal-manipulation/src/ops.rs
+++ padamo-signal-manipulation/src/ops.rs
@@ -30,10 +30,22 @@ pub struct LazySpaceConverter{
     divider:usize,
     source:LazyDetectorSignal,
     is_sum:bool,
+    frame_shape:Vec<usize>
 }
 
 impl LazySpaceConverter {
-    pub fn new(divider: usize, source: LazyDetectorSignal, is_sum:bool) -> Self { Self { divider, source ,is_sum} }
+    pub fn new(divider: usize, source: LazyDetectorSignal, is_sum:bool) -> Self {
+        let frame_shape:Vec<usize> = if source.length()>0{
+            let mut testframe_size = source.request_range(0,1).shape;
+            testframe_size.drain(1..).collect()
+        }
+        else{
+            vec![1]
+        };
+
+        Self { divider, source ,is_sum, frame_shape}
+
+    }
 }
 
 fn compress<D:ndarray::Dimension+ndarray::RemoveAxis>(x:ndarray::ArrayBase<ndarray::ViewRepr<&f64>,D>, is_sum:bool)->ndarray::ArrayBase<ndarray::OwnedRepr<f64>,D::Smaller>{
@@ -60,50 +72,81 @@ impl LazyArrayOperation<ArrayND<f64>> for LazySpaceConverter{
         let start_src = start*self.divider;
         let end_src = end*self.divider;
         let divider = self.divider;
-        let raw_data:ArrayND<f64> = self.source.request_range(start_src,end_src);
         let is_sum = self.is_sum;
-        let frame_size = raw_data.shape.iter().skip(1).fold(1usize,|a,b| a*b);
-        // let stepped_data:Vec<_> = (0..end-start).par_bridge()
-        //     .map(|i| raw_data.slice(ndarray::s![i*divider..(i+1)*divider,..,..]))
-        //     .map(|x| compress(x, is_sum))
-        //     .collect();
-
-        let mut tgt_shape = raw_data.shape.clone();
-        tgt_shape[0] = end-start;
-
+        // let raw_data:ArrayND<f64> = self.source.request_range(start_src,end_src);
+        let mut tgt_shape = vec![end-start];
+        tgt_shape.extend(&self.frame_shape);
+        let target = Arc::new(Mutex::new(ArrayND::<f64>::new(tgt_shape.into(),0.0)));
         let threadcount = num_cpus::get();
         let mut threads:VecDeque<thread::JoinHandle<()>> = VecDeque::with_capacity(10);
-
-        let target = Arc::new(Mutex::new(ArrayND::<f64>::new(tgt_shape.into(),0.0)));
-        let source = Arc::new(raw_data);
-
-        for pixel in 0usize..frame_size{
-            free_threads(&mut threads, threadcount);
-
-            let length = end-start;
-            let src = source.clone();
-            let tgt = target.clone();
-
-            let handle = thread::spawn(move || {
-                for i in 0..length{
-                    let mut sum:f64 = 0.0;
-                    for j in i*divider..i*divider+divider{
+        let frame_size = self.frame_shape.iter().fold(1usize,|a,b| a*b);
+        for frame in 0..end-start{
+            let src_part = Arc::new(self.source.request_range((start+frame)*self.divider,(start+frame+1)*self.divider));
+            for pixel in 0..frame_size{
+                // let length = end-start;
+                let part_length = divider;
+                let src = src_part.clone();
+                let tgt = target.clone();
+                free_threads(&mut threads, threadcount);
+                let handle = thread::spawn(move || {
+                    let mut sum = 0.0;
+                    for j in 0..part_length{
                         sum += src.flat_data[j*frame_size+pixel];
                     }
                     if !is_sum{
                         sum/=divider as f64;
                     }
-                    tgt.lock().unwrap().flat_data[i*frame_size+pixel] = sum;
-                }
-            });
-
-            threads.push_back(handle);
+                    tgt.lock().unwrap().flat_data[frame*frame_size+pixel] = sum;
+                });
+                threads.push_back(handle);
+            }
         }
-
         free_threads(&mut threads, 1);
-
         let lock = Arc::try_unwrap(target).unwrap();
         lock.into_inner().unwrap()
+
+        // let frame_size = raw_data.shape.iter().skip(1).fold(1usize,|a,b| a*b);
+        // // let stepped_data:Vec<_> = (0..end-start).par_bridge()
+        // //     .map(|i| raw_data.slice(ndarray::s![i*divider..(i+1)*divider,..,..]))
+        // //     .map(|x| compress(x, is_sum))
+        // //     .collect();
+        //
+        // let mut tgt_shape = raw_data.shape.clone();
+        // tgt_shape[0] = end-start;
+        //
+        // let threadcount = num_cpus::get();
+        // let mut threads:VecDeque<thread::JoinHandle<()>> = VecDeque::with_capacity(10);
+        //
+        // let target = Arc::new(Mutex::new(ArrayND::<f64>::new(tgt_shape.into(),0.0)));
+        // let source = Arc::new(raw_data);
+        //
+        // for pixel in 0usize..frame_size{
+        //     free_threads(&mut threads, threadcount);
+        //
+        //     let length = end-start;
+        //     let src = source.clone();
+        //     let tgt = target.clone();
+        //
+        //     let handle = thread::spawn(move || {
+        //         for i in 0..length{
+        //             let mut sum:f64 = 0.0;
+        //             for j in i*divider..i*divider+divider{
+        //                 sum += src.flat_data[j*frame_size+pixel];
+        //             }
+        //             if !is_sum{
+        //                 sum/=divider as f64;
+        //             }
+        //             tgt.lock().unwrap().flat_data[i*frame_size+pixel] = sum;
+        //         }
+        //     });
+        //
+        //     threads.push_back(handle);
+        // }
+        //
+        // free_threads(&mut threads, 1);
+        //
+        // let lock = Arc::try_unwrap(target).unwrap();
+        // lock.into_inner().unwrap()
 
         //let views:Vec<_> = stepped_data.iter().map(|x| x.view()).collect();
         //let res_data = ndarray::stack(ndarray::Axis(0), &views).unwrap();
