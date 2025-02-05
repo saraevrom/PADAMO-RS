49348d27a4678264cf3cb8203772b9fdbfe804ce
Roman Saraev <saraevrom@gmail.com>
2025-02-05 19:34:36 +0300

    Added rhai_sci to detectors

--- padamo-detectors/src/scripted/mod.rs
+++ padamo-detectors/src/scripted/mod.rs
@@ -1,12 +1,14 @@
 
 use crate::polygon::DetectorContent;
-use rhai::{Engine, EvalAltResult};
+use rhai::{Engine, EvalAltResult, packages::Package};
+use rhai_sci::SciPackage;
 // pub mod errors;
 
 pub fn parse_scripted(src:&str)->Result<DetectorContent,Box<EvalAltResult>>{
     let mut engine = Engine::new();
     engine.build_type::<crate::polygon::DetectorPixel>();
     engine.build_type::<crate::polygon::DetectorContent>();
+    engine.register_global_module(SciPackage::new().as_shared_module());
     let res:DetectorContent = engine.eval(src)?;
     // let res1:DetectorContent = from_dynamic(&res)?;
     Ok(res)
