fc3db828cc8e336b3b156a42a98c78ae7595f84c
Roman Saraev <saraevrom@gmail.com>
2025-05-06 12:37:59 +0300

    Fixed autoscale in view

--- padamo-detectors/src/lib.rs
+++ padamo-detectors/src/lib.rs
@@ -349,19 +349,39 @@ pub enum Scaling{
 
 impl Scaling{
     pub fn get_bounds(&self, frame:&ArrayND<f64>, alive_pixels:&ArrayND<bool>)->(f64,f64){
-        let first = frame.flat_data.get(0).map(|x|*x).unwrap_or(0.0);
         match self{
             Self::Autoscale=>{
-                let (min,max) = frame.flat_data.iter()
-                    .enumerate()
-                    .filter(|x| alive_pixels.flat_data.get(x.0).map(|y|*y).unwrap_or(false))
-                    .map(|x| x.1)
-                    .fold((first,first), |a,b| (a.0.min(*b),a.1.max(*b)));
-                if max<=min{
-                    (min-0.1,max+0.1)
-                }
-                else{
-                    (min,max)
+
+                // let (min,max) = frame.flat_data.iter()
+                //     .enumerate()
+                //     .filter(|x| alive_pixels.flat_data.get(x.0).map(|y|*y).unwrap_or(false))
+                //     .map(|x| x.1)
+                //     .fold((first,first), |a,b| (a.0.min(*b),a.1.max(*b)));
+                let min = frame.flat_data
+                        .iter()
+                        .enumerate()
+                        .filter(|x| alive_pixels.flat_data.get(x.0).map(|y|*y).unwrap_or(false))
+                        .min_by(|a,b| a.1.total_cmp(b.1))
+                        .map(|x| x.1);
+                let max = frame.flat_data
+                        .iter()
+                        .enumerate()
+                        .filter(|x| alive_pixels.flat_data.get(x.0).map(|y|*y).unwrap_or(false))
+                        .max_by(|a,b| a.1.total_cmp(b.1))
+                        .map(|x| x.1);
+                match (min, max){
+                    (Some(l),Some(u)) => {
+                        if max<=min{
+                            (l-0.1,l+0.1)
+                        }
+                        else{
+                            (*l,*u)
+                        }
+                    }
+                    _ =>{
+                        (-0.1, 0.1)
+                    }
+
                 }
             }
             Self::Fixed(min, max)=>{
