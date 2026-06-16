6bc5f98198751aed3a93e6468acacce29c607dab
Roman Saraev <saraevrom@gmail.com>
2026-06-16 18:51:42 +0300

    Switched back to h264 encoder for videos

--- builder-lite.sh
+++ builder-lite.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./universal_build.sh release $@ --no-default-features --features viewer --features plotter --features build-ffmpeg
+./universal_build.sh release $@ --no-default-features --features viewer --features plotter
--- builder-release.sh
+++ builder-release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./universal_build.sh release $@ --features build-ffmpeg
+./universal_build.sh release $@
